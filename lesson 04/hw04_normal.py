9631601c137601d7a58152d7f3ea9f906cdc5f55
1dNt <ivoron.spb@gmail.com>
2019-07-05 16:05:18 +0300

    Complete lesson 4

---
+++ lesson 04/hw04_normal.py
@@ -0,0 +1,144 @@
+# Задание-1:
+# Вывести символы в нижнем регистре, которые находятся вокруг
+# 1 или более символов в верхнем регистре.
+# Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
+# Решить задачу двумя способами: с помощью re и без.
+print('------------\nЗадача-1:\n')
+line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
+       'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
+       'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
+       'LxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIVjXa'\
+       'pzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnWete'\
+       'kUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfCvzQ'\
+       'WrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXb'\
+       'JrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCC'\
+       'EUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfB'\
+       'tQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuT'\
+       'SkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCfKCu'\
+       'UJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWHuXB'\
+       'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
+       'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
+       'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+
+import re
+
+l1 = re.findall(r'[^A-Z]+', line)
+print(l1)
+
+# var.2
+l = str()
+
+for id in line:
+       if id.islower():
+              l += id
+       else:
+              l += '_'
+l2 = [id for id in l.split('_') if id]
+print(l2)
+
+# Задание-2:
+# Вывести символы в верхнем регистре, слева от которых находятся
+# два символа в нижнем регистре, а справа - два символа в верхнем регистре.
+# Т.е. из строки 
+# "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
+# нужно получить список строк: ['AY', 'NOGI', 'P']
+# Решить задачу двумя способами: с помощью re и без.
+print('------------\nЗадача-2:\n')
+line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
+       'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'\
+       'fzKTUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSA'\
+       'HqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIV'\
+       'jXapzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnW'\
+       'etekUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfC'\
+       'vzQWrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbR'\
+       'uXbJrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkm'\
+       'jCCEUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOn'\
+       'LfBtQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGS'\
+       'euTSkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCf'\
+       'KCuUJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWH'\
+       'uXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQN'\
+       'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
+       'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+
+l2 = re.findall(r'[a-z]{2}([A-Z]+)[A-Z]{2}', line_2)
+print(l2)
+
+# var.2
+l = str()
+for i, id in enumerate(line_2):
+       if id.isupper() and line_2[i-2:i].islower():
+              l += ('@' + id)
+       elif id.isupper():
+              l += id
+       else:
+              l += '_'
+l2 = [id[1:-2] for id in l.split('_') if (id and id[0] is '@' and len(id) > 3)]
+print(l2)
+
+
+# Задание-3:
+# Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
+# произвольными целыми цифрами, в результате в файле должно быть
+# 2500-значное произвольное число.
+# Найдите и выведите самую длинную последовательность одинаковых цифр
+# в вышезаполненном файле.
+print('------------\nЗадача-3:\n')
+
+import random
+
+def cnt_num(num):
+       """
+       фукция определения разрядности числа
+       :param num: любое число int
+       :return: количество цифр int
+       """
+       cnt = 0
+       while num != 0:
+              cnt += 1
+              num = num // 10
+       return cnt
+
+def rand_num(n=5):
+       """
+       Функция рандомного положительного N-значного числа
+       :param n: Разрядность числа (int), 5 is default
+       :return: int
+       """
+       try:
+              return random.randint(10**(n-1), 10**(n)-1)
+       except ValueError:
+              print('Неправильная разрядность числа. Посчитано для n - default')
+              return rand_num()
+
+def sequence_list(num):
+       """
+       Функция формирует список из элементов состоящих из одинаковых цифр
+       :param num: любой число int
+       :return: list
+       """
+       i = 1
+       n = -1
+       f_list = []
+       while i <= cnt_num(num):
+              if num//10**(i-1)%10 == num//10**(i-2)%10 and type(num//10**(i-2)%10) is int:
+                     f_list[n] += (num // 10 ** (i - 1) % 10)*10**k
+                     i += 1
+                     k += 1
+              else:
+                     f_list.append(num // 10 ** (i - 1) % 10)
+                     k = 1
+                     n += 1
+                     i += 1
+       return f_list
+
+file = open('hw04_norm_task3.txt', 'w', encoding='UTF-8')
+
+rnum = rand_num(250)
+lst = sequence_list(rnum)
+max_sq = max(list(map(list, zip([cnt_num(id) for id in lst], lst))))
+
+file.write(f'Число: \n{rnum}\n\n'
+           f'Максимальная последовательность в числе состоит из: {max_sq[0]} цифр.\n'
+           f'Послеловетельность: {max_sq[1]}')
+
+file.close()
