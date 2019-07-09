893c2605dfd5d0e434e510ed50d1312b89f2c368
1dNt <ivoron.spb@gmail.com>
2019-07-09 23:01:37 +0300

    Complete lesson 5 (normal/ez)

--- lesson 05/hw05_easy.py
+++ lesson 05/hw05_easy.py
@@ -3,8 +3,73 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+import os
+
+def get_dir_name():
+    """
+    Выделяет список имен папок из введенных данных
+    :return: list
+    """
+    temp = input('Введите названия папок, через пробел:\n')
+    return temp.split(' ')
+
+def make_dir(*f_args):
+    """
+    Функция создания каталогов в текущей директории
+    Создает N каталогов, явно перечисленных на входе f_args
+    :param f_args: *list
+    :return: None
+    """
+    if f_args:
+        idx = str()
+        for id in f_args:
+            dir_path = os.path.join(os.getcwd(), str(id))
+            try:
+                if id:
+                    os.mkdir(dir_path)
+                    idx += f'{id} '
+            except FileExistsError:
+                print(f'Директория *\\{id} уже существует')
+        if idx:
+            print(f'\nКаталоги созданы: {idx}')
+    else:
+        make_dir(*get_dir_name())
+
+def del_dir(*f_args):
+    """
+    Функция удаления каталогов в текущей директории
+    Удаляет N каталогов, явно перечисленных на входе f_args
+    :param f_args: *list
+    :return: None
+    """
+    if f_args:
+        idx = str()
+        for id in f_args:
+            dir_path = os.path.join(os.getcwd(), str(id))
+            try:
+                if id:
+                    os.rmdir(dir_path)
+                    idx += f'{id} '
+            except FileNotFoundError:
+                print(f'Директория *\\{id} не найдена')
+        if idx:
+            print(f'\nКаталоги удалены: {idx}')
+    else:
+        del_dir(*get_dir_name())
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
+def view_dir(*args):
+    """
+    Функция просмотра содержимого директории
+    :param args: Any (не используется, заглушка)
+    :return: None
+    """
+    print('Содержимое директории:')
+    for id in os.listdir():
+        print(id)
+
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
--- lesson 05/hw05_normal.py
+++ lesson 05/hw05_normal.py
@@ -1,4 +1,4 @@
-# Задача-1:
+﻿# Задача-1:
 # Напишите небольшую консольную утилиту,
 # позволяющую работать с папками текущей директории.
 # Утилита должна иметь меню выбора действия, в котором будут пункты:
@@ -13,3 +13,106 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+
+import os
+import sys
+import hw05_easy as hw5
+
+args = sys.argv
+
+def menu(*f_args):
+    """
+    Меню ключе в консоли.
+    :param f_args: заглушка
+    :return: None
+    """
+    print("""__________________________________________
+Доступные ключи:
+h: Справка.
+cd *<имя директории>: Перейти в папку в текущей директории.
+up: Вернуться на папку назад.
+dir: Просмотреть содержимое текущей папки.
+del *<перечень директорий>: Удалить папку.
+make *<перечень директорий>: Создать папку.
+Q: Выход.
+
+* - не обязательный параметр
+__________________________________________""")
+
+def get_key(arg):
+    """
+    Выделяет ключ из списка введенных данных
+    :param arg: list
+    :return: list
+    """
+    try:
+        return arg[0]
+    except IndexError:
+        return None
+
+def get_dir_name1(arg):
+    """
+    Выделяет список имен папок из введенных данных
+    :param arg: list
+    :return: list
+    """
+    try:
+        return arg[1:]
+    except IndexError:
+        return None
+
+def change_dir(*f_args):
+    """
+    Функция изменения директории, с запросом имени подпапки
+    :param f_args: имя вложенного каталога list
+    :return: None
+    """
+    if f_args:
+        try:
+            os.chdir(os.path.join(os.getcwd(), str(f_args[0])))
+            print(f'Успешно: {os.getcwd()}')
+        except FileNotFoundError:
+            print(f'Директория *\\{f_args[0]} не найдена')
+    else:
+        change_dir(input('Введите название папки:\n'))
+
+def return_dir():
+    """
+    Функция возврата на папку вверх по каталогу
+    :return: None
+    """
+    os.chdir(os.path.dirname(os.getcwd()))
+    print(f'Успешно: {os.getcwd()}')
+
+
+print(sys.argv)
+
+try:
+    key = sys.argv[1]
+except IndexError:
+    key = None
+try:
+    dir_name = sys.argv[2:]
+except IndexError:
+    dir_name = None
+
+do = {
+    'h': menu,
+    'cd': change_dir,
+    'up': return_dir,
+    'dir': hw5.view_dir,
+    'del': hw5.del_dir,
+    'make': hw5.make_dir
+}
+
+while True:
+    if key == 'Q':
+        break
+    elif do.get(key):
+        do[key](*dir_name)
+        key = dir_name = None
+    else:
+        ans = input('\nУкажите ключ. (h - для получения справки)\n')
+        key = get_key(ans.split(' '))
+        dir_name = get_dir_name1(ans.split(' '))
